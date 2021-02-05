836abb84acd1b301898fd423b0ae8049a75c12bb
Mingyu Kim <68273065+MingyuKim-2933@users.noreply.github.com>
2021-02-05 12:58:51 +0900

    Add files via upload

--- 데이터 조회와 필터링/실습 2(01~10).sql
+++ 데이터 조회와 필터링/실습 2(01~10).sql
@@ -52,6 +52,17 @@ from
 	address
 where
 	postal_code like '77%'
+	
+-- 문제7번) address 테이블을 이용하여, 우편번호(postal_code) 값이  두번째글자가 1인 우편번호의  address_id, address, district ,postal_code  컬럼을 확인해주세요.
+select
+	address_id,
+	address,
+	district ,
+	postal_code
+from
+	address
+where
+	postal_code like '_1%'
 
 -- 문제8번) payment 테이블을 이용하여,  고객번호가 341에 해당 하는 사람이 결제를 2007년 2월 15~16일 사이에 한 모든 결제내역을 확인해주세요.	
 select 
